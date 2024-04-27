cb5ecc21f018d4b372099eea34a6f4a57830b895
Anderson Krahl <ander_uo@hotmail.com>
2024-04-27 18:54:26 -0300

    facts improvement + performance

--- backend/cmd/routes.go
+++ backend/cmd/routes.go
@@ -10,4 +10,5 @@ func setupRoutes(app *fiber.App) {
 	app.Get("/", handlers.ListFacts)
 	app.Get("/fact/:id", handlers.ShowFactById)
 	app.Post("/fact", handlers.CreateFact)
+	app.Delete("/fact/:id", handlers.DeleteFactById)
 }
--- backend/handlers/facts.go
+++ backend/handlers/facts.go
@@ -8,25 +8,33 @@ import (
 )
 
 func ListFacts(c *fiber.Ctx) error {
-	//facts := []models.Fact{}
-	//database.DB.Db.Find(&facts)
+	facts := []models.Fact{}
+	database.DB.Db.Find(&facts)
 
-	//return c.Status(200).JSON(facts)
-	return c.Status(200).JSON("DASDAS")
+	return c.Status(200).JSON(facts)
 }
 
 func ShowFactById(c *fiber.Ctx) error {
-	fact := models.Fact{}
 	id := c.Params("id")
+	fact := models.Fact{}
+	if err := database.DB.Db.Where("id = ?", id).First(&fact).Error; err != nil {
+		return c.Status(404).SendString("Fact not found")
+	}
+	return c.Status(200).JSON(fact)
+}
 
-	result := database.DB.Db.Where("id = ?", id).First(&fact)
-	if err := c.BodyParser(fact); err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"message": err.Error(),
-		})
+func DeleteFactById(c *fiber.Ctx) error {
+	id := c.Params("id")
+	fact := models.Fact{}
+	if err := database.DB.Db.Where("id = ?", id).First(&fact).Error; err != nil {
+		return c.Status(404).JSON("Fact not found")
+	}
+	// Delete the fact
+	if err := database.DB.Db.Delete(&fact).Error; err != nil {
+		return c.Status(500).JSON("Failed to delete the fact")
 	}
 
-	return c.Status(fiber.StatusOK).JSON(result)
+	return c.Status(204).JSON("Fact deleted successfully")
 }
 
 func CreateFact(c *fiber.Ctx) error {
