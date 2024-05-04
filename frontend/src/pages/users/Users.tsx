4b39c362f1886f047205a90954bd38760aaba87a
arthur <arthur.wieb@gmail.com>
2024-05-04 14:55:36 -0300

    modal em usuarios

--- frontend/src/pages/users/Users.tsx
+++ frontend/src/pages/users/Users.tsx
@@ -1,15 +1,19 @@
 import DataTable from "../../components/dataTable/DataTable"
 import "./users.scss"
 import { userRows, userColumns } from "../../data";
+import { useState } from "react";
+import Add from "../../components/add/Add";
 
 export const Users = () => {
+  const [open, setOpen] = useState(false)
   return (
     <div className="users">
       <div className="info">
         <h1>Users</h1>
-        <button>Add New User</button>
+        <button onClick={() => setOpen(true)}>Add New User</button>
       </div>
       <DataTable slug="users" columns={userColumns} rows={userRows} />
+      {open && <Add slug="users" columns={userColumns} setOpen={setOpen} />}
     </div>
   )
 }
